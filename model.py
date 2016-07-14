fb2af2ded9fdb721aff82bb4b3e3cadd34e5f37c
Nitin Verma <nitin.d.verma@pwc.com>
2016-07-14 07:47:52 +0000

    Adding delete function, closing session after every call.

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ from config import SQLConfig
 from sqlalchemy import (Column, Integer, String, ForeignKey, Text, DateTime,
                         Time, Boolean)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import backref, mapper, relation, sessionmaker,relationship
 from sql_connector import SqlConnector
 
 
@@ -12,36 +11,46 @@ Base = declarative_base()
 #connector = SqlConnector('root', '10.132.0.3', 'clouddr', password='root')
 connector = SqlConnector(
     SQLConfig.USER_ID, SQLConfig.IP, SQLConfig.DB, password=SQLConfig.PASSWORD)
-session = connector.get_sql_session()
+
 
 
 class Guests(Base):
-  __tablename__ = "Guests"
-  id = Column(String(50),primary_key=True)
-  first = Column(String(20),nullable=False)
-  last = Column(String(20),nullable=False)
+    __tablename__ = "Guests"
+    id = Column(String(50),primary_key=True)
+    first = Column(String(20),nullable=False)
+    last = Column(String(20),nullable=False)
 
 
 def AllGuests():
-  return session.query(Guests).all()
+    session = connector.get_sql_session()
+    result = session.query(Guests).all()
+    session.close()
+    return result
 
 
 def UpdateGuest(id, first, last):
-  row = session.query(Guests).filter(id == id).first()
-  row.first = first
-  row.last = last
-  session.add(row)
-  session.commit()
-  return row
+    session = connector.get_sql_session()
+    row = session.query(Guests).filter(id == id).first()
+    row.first = first
+    row.last = last
+    session.add(row)
+    session.commit()
+    session.close()
+    return row
 
 
 def InsertGuest(id, first, last):
-  guest = Guests(id=id, first=first, last=last)
-  session.add(guest)
-  session.commit()
-  return guest
+    session = connector.get_sql_session()
+    guest = Guests(id=id, first=first, last=last)
+    session.add(guest)
+    session.commit()
+    session.close()
+    return guest
 
 
-"""def DeleteGuest(id):
-  key = ndb.Key(Guest, id)
-  key.delete()"""
+def DeleteGuest(id):
+    session = connector.get_sql_session()
+    row = session.query(Guests).filter(id == id).first()
+    session.add(row)
+    session.commit()
+    session.close()
